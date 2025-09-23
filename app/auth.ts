c20e3a6becb300a951136f82a6685e3e16ec861e
Zanzibar <zanzisworld42@gmail.com>
2025-09-23 12:23:58 -0700

    Auth with database is working

--- app/auth.ts
+++ app/auth.ts
@@ -1,9 +1,19 @@
+import PostgresAdapter from '@auth/pg-adapter'
+import { Pool } from 'pg'
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
-import Github from 'next-auth/providers/github'
-import Discord from 'next-auth/providers/discord'
-import Twitter from 'next-auth/providers/twitter'
+// import Github from 'next-auth/providers/github'
+// import Discord from 'next-auth/providers/discord'
+// import Twitter from 'next-auth/providers/twitter'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Github, Discord, Twitter],
+  adapter: PostgresAdapter(pool),
+  providers: [Google],
 })
