32d76467a846bd861a3d3f86536230f53a9e8466
Zanzibar <zanzisworld42@gmail.com>
2025-10-04 15:55:01 -0700

    Toggle between picking and paying.

--- app/(main)/join/level-up/PickAndPay.tsx
+++ app/(main)/join/level-up/PickAndPay.tsx
@@ -1,22 +1,41 @@
+import { useState } from 'react'
 import Benefits from './Benefits'
 import PlanOptions from './PlanOptions'
+import AtomicButton from './AtomicButton'
 
 function PickAndPay() {
+  const [prodOption, setProdOption] = useState('')
+  const [isGettingPayment, setIsGettingPayment] = useState(false)
+
   function handlePayment(renewal: string) {
     console.log('Time to collect big $$: ' + renewal)
+    setProdOption(renewal)
+    setIsGettingPayment(true)
   }
 
   return (
     <div className='min-h-screen max-w-4xl mx-auto bg-background py-12'>
       <h1 className='mb-6'>Become a Charter Member</h1>
-      <div className='space-y-4'>
-        <p className='text-center text-lg'>
-          As a charter member, you help us power the platform that empowers the
-          people. Your support matters.
-        </p>
-        <Benefits />
-        <PlanOptions onSelect={handlePayment} />
-      </div>
+      {isGettingPayment ? (
+        <div className='text-center'>
+          <h2>Get paid</h2>
+          <AtomicButton
+            label='Pay'
+            onClick={() => {
+              setIsGettingPayment(false)
+            }}
+          />
+        </div>
+      ) : (
+        <div className='space-y-4'>
+          <p className='text-center text-lg'>
+            As a charter member, you help us power the platform that empowers
+            the people. Your support matters.
+          </p>
+          <Benefits />
+          <PlanOptions onSelect={handlePayment} />
+        </div>
+      )}
     </div>
   )
 }
